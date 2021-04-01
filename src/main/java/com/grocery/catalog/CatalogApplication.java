531502097fd0636e8642e2185f5a7cb4771792f4
anandkrish4204 <anandkrish@humworld.in>
2021-04-01 09:03:07 +0530

    Configured Spring Gateway

--- src/main/java/com/grocery/catalog/CatalogApplication.java
+++ src/main/java/com/grocery/catalog/CatalogApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CatalogApplication {
 
     public static void main(String[] args) {
